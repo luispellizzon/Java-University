211494c868f91b8e3a113285f2c5fb7042c795a8
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-03-16 21:11:09 +0000

    Checking user Age input

--- Workspace/Assigment2Java/src/policy/HealthPolicy.java
+++ Workspace/Assigment2Java/src/policy/HealthPolicy.java
@@ -2,8 +2,10 @@ package policy;
 
 public class HealthPolicy {
 
-	private float basicCost;
-	private byte age;
+	private short basicCost; // +80 if age > 50 // 0 < 18
+	private byte age, dependantNumber; //dependant 1=250 2=150 3=100 4=50 5> free up to 6
+	private String dependantNamem, userName; // dep name < 18
+	private boolean inpatient, ageInput; // +200 euro normal + 100 for private
 	
 	byte getAge () {
 		return age;
@@ -13,11 +15,30 @@ public class HealthPolicy {
 		basicCost = 426;
 	}
 	
-	public void readAge () {
-		System.out.println("Luis");
+	public void readAge() {
+		ageInput = false;
+		
+		while (ageInput != true) {
+			try {
+				System.out.print("Please, Enter Your Age:");
+				age = SkillsDemo2Main.reader.nextByte();
+				
+				if(age < 0) {
+					throw new Exception();
+				} else {
+					ageInput = true;	
+				}
+			} catch (Exception e) {
+				System.out.println("Please, Enter Your Age In Positive Numbers\n");
+				SkillsDemo2Main.reader.nextLine();
+				ageInput = false;
+			}
+		}
 	}
 	
 	public void readName () {
+		System.out.print("Please, Enter Your Name:");
+		userName = SkillsDemo2Main.reader.nextLine();
 		
 	}
 	
