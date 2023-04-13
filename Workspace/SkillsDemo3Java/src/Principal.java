dc5bcc63a6c2b84713bb0ea565051c623c686ad0
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-04-13 13:22:48 -0300

    Principal Class Finished

--- Workspace/SkillsDemo3Java/src/Principal.java
+++ Workspace/SkillsDemo3Java/src/Principal.java
@@ -3,9 +3,36 @@ public class Principal extends Employee {
 	private float principalBasicSalary = 5000.00f;
 	private int teachersNumber;
 	
-	public void getNumberOfTeachers() {
-		//get number of teachers
-		System.out.println("Get Number function");
+	public Principal(){
+		setSalary(principalBasicSalary);
+	}
+	
+	//get number of teachers
+	protected void getNumberOfTeachers() {
+		boolean teachersNumberInput = false;
+		while(teachersNumberInput != true) {
+			try {
+				System.out.print("\nEnter Number Of Teachers In The School: ");
+				teachersNumber = PaymentCalculator.reader.nextInt();
+				if(teachersNumber >= 200) {
+					setAdditionalPayment(1196.67f);
+				}
+				else if(teachersNumber >= 100){
+					setAdditionalPayment(1019.83f);
+				}
+				else if(teachersNumber >= 10) {
+					setAdditionalPayment(869.33f);
+				}
+				else {
+					setAdditionalPayment(775.83f);
+				}
+				teachersNumberInput = true;
+			} catch(Exception e){
+				System.out.println("Not valid");
+				PaymentCalculator.reader.nextLine();
+				teachersNumberInput = false;
+			}
+		}
 	}
 	
 }
