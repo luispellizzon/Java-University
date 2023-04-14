8edde460ac9ac202e5ee3dd66b4dbaaacbc1691d
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-04-14 17:47:49 -0300

    New Class to determine console font and colors styles for the program

---
+++ Workspace/SkillsDemo3Java/src/Styles.java
@@ -0,0 +1,9 @@
+
+public class Styles {
+	public static final String BLACK_BOLD = "\033[1;30m";
+	public static final String RESET_STYLE = "\u001B[0m";
+	public static final String UNDERLINE = "\033[4;30m";
+	public static final String ERROR_MESSAGE_COLOR = "\u001B[31m";
+	public static final String PURPLE_BOLD = "\033[1;35m";
+
+}
